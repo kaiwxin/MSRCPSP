f168814de951ccbd9e8172b56e4ef683e7fafb33
kaiwxin <xiongkai1223@gmail.com>
2019-01-01 15:24:11 +0800

    modify the mutation

--- dynamic_msrcpsp/src/evolutionary_algorithms/differential_evolution_and_greedy_algorithm/mutation/Mutation.java
+++ dynamic_msrcpsp/src/evolutionary_algorithms/differential_evolution_and_greedy_algorithm/mutation/Mutation.java
@@ -9,6 +9,7 @@ import evolutionary_algorithms.CommonUtil;
 import project.Project;
 import scheduling.BaseIndividual;
 import scheduling.BasePopulation;
+import scheduling.Schedule;
 
 /**
  * 差分进化算法的变异算子
@@ -26,11 +27,51 @@ public class Mutation {
     /**
      * 变异操作
      * 随机从种群中选择三个不同的个体对当前个体进行变异进化
-     * 对变异后的基因染色体进行修复，使其满足技能要求
+     * 对变异后的任务链表和资源链表分别进行修复，使其满足要求
      * @param pop  种群
-     * @return  返回修复后显型染色体集合      
+     * @return  返回修复后任务-资源链表集合      
      */
-    public List<int[]> mutation(BasePopulation pop){
+    public List<int[][]> mutation(BasePopulation pop){
+        List<int[][]> task_resourceLists=new ArrayList<>();
+        BaseIndividual[] oldPop=pop.getPopulation();
+        for(int i=0;i<pop.size();i++){
+            //待变异的个体
+            BaseIndividual parent=oldPop[i];
+            Schedule scheduleOfParent=parent.getSchedule();
+            int[] taskList=scheduleOfParent.getTaskList();
+            int[] resourceList=scheduleOfParent.getResourceList();
+
+            //从（pop-1）种群中随机选择三个不相同的个体
+            HashSet<Integer> set=new HashSet<>();
+            set.add(i);
+            randomSet(0,pop.size()-1,4,set);
+            set.remove(i);
+            Stack<Integer> stack=new Stack<>();
+            for(int index:set){
+                stack.push(index);
+            }
+            BaseIndividual indiv1=oldPop[stack.pop()];
+            BaseIndividual indiv2=oldPop[stack.pop()];
+            BaseIndividual indiv3=oldPop[stack.pop()];
+            int[] taskList_1=indiv1.getSchedule().getTaskList();
+            int[] resourceList_1=indiv1.getSchedule().getResourceList();
+            int[] taskList_2=indiv2.getSchedule().getTaskList();
+            int[] resourceList_2=indiv2.getSchedule().getResourceList();
+            int[] taskList_3=indiv3.getSchedule().getTaskList();
+            int[] resourceList_3=indiv3.getSchedule().getResourceList();
+
+            //任务链表进行变异，此处用于变异的元素是任务的执行顺序号
+            //比如任务执行顺序是[T1,T4,T2,T3],那么任务集{T1,T2,T3,T4}的执行顺序号对应为[1,3,4,2]
+
+
+            //资源链表变异
+
+
+        }
+        return task_resourceLists;
+    }
+
+    public List<int[]> mutationpOfResourceList(BasePopulation pop){
         
         List<int[]> phenotypeList=new ArrayList<>();
         BaseIndividual[] oldPop=pop.getPopulation();
